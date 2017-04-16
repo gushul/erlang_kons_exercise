6b70b1dbd507d4c8575a0bc07286e3dfc41e86fc
Oleg Gushul <gushul.o@gmail.com>
2017-04-16 09:46:23 +0300

    First day

--- src/about_pattern_matching.erl
+++ src/about_pattern_matching.erl
@@ -9,42 +9,42 @@
 
 what_looks_like_assignment_might_not_be() ->
     Fruits = apple,
-    Fruits = __.
+    Fruits = apple.
 
 assignment_with_a_tuple() ->
     {X, Y} = {banana, apple},
-    (X =:= __) and (Y =:= apple).
+    (X =:= banana) and (Y =:= apple).
 
 branching_on_patterns() ->
     Fruits = {banana, apple},
     case Fruits of
         {banana, Item} ->
-            __ =:= Item
+            apple =:= Item
     end.
 
 sometimes_we_do_not_need_to_know_everything() ->
-    Fruits = {__, banana},
+    Fruits = { apple, banana},
     case Fruits of
-        {apple, _} ->
+        {apple, banana} ->
             get_here
     end.
 
 and_sometimes_we_dont_need_to_match_anything() ->
     case {banana, apple} of
-        {pear, _} ->
+        {pear, apple} ->
             Answer = pear;
-        _ ->
+        {banana, apple} ->
             Answer = anything
     end,
-    __ =:= Answer.
+    anything =:= Answer.
 
 we_can_find_patterns_in_arguments() ->
-    match_the_argument(__).
+    match_the_argument(bar).
 
 match_the_argument(foo) ->
     wrong;
 match_the_argument(bar) ->
     right;
-match_the_argument(_) ->
+match_the_argument(wronger) ->
     wronger.
 
